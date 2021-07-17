9facaf0773dbf9989504cd5f0c5ffbd7b585a6b3
ezzimo <ezzi.mohammed1@yahoo.fr>
2021-07-17 22:42:02 +0100

    rebuilding the DB schema and models

--- Ecommerce_Store/admin.py
+++ Ecommerce_Store/admin.py
@@ -5,13 +5,23 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
-    list_filter = ['in_stock', 'is_active']
-    list_editable = ['price', 'in_stock']
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = [
+        "title",
+        "product_type",
+        "category",
+        "slug",
+        "description",
+        "regular_price",
+        "is_active",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["title", "is_active"]
+    list_editable = ["regular_price", "is_active"]
+    prepopulated_fields = {"slug": ("title",)}
